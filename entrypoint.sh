68d957bf04de589084fc2e9000c22ae5529a6b20
Vladimir Levin <vladimirlevinozinki@gmail.com>
2023-08-07 18:22:23 +0900

    Bash fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,7 @@ if [[ "$YA_ACTION" == "reapply" ]]; then
   action_option="reapply"
 fi
 
-oro "$action_option" --root $YA_ROOT --registry $YA_REGISTRY
+echo ${action_option:+"$action_option"}
+echo "--root $YA_ROOT --registry $YA_REGISTRY"
+
+oro ${action_option:+"$action_option"} --root $YA_ROOT --registry $YA_REGISTRY
